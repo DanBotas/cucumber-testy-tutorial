2dec6aa91afc45b569bcfd3a3049a48a9193951b
Dan Botas <botas.dan@gmail.com>
2017-02-16 20:40:00 +0200

    use tablerow for container

--- src/test/java/org/fasttrackit/automation/TablesTest.java
+++ src/test/java/org/fasttrackit/automation/TablesTest.java
@@ -1,6 +1,10 @@
 package org.fasttrackit.automation;
 
 import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.form.CheckBox;
+import com.sdl.selenium.web.table.Cell;
+import com.sdl.selenium.web.table.Row;
+import com.sdl.selenium.web.table.Table;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -30,5 +34,18 @@ public class TablesTest extends TestBase {
         checkbox.click();
     }
 
+    @Test
+    public void selectRowByEmail(){
+        openLoginPage();
+        loginView.login("eu@fast.com", "eu.pass");
+
+        Table table = new Table();
+
+        Row row = table.getRow(new Cell(4,"davidmiller@mail.com"));
+
+        CheckBox checkbox = new CheckBox(row);
+        checkbox.click();
+
+    }
 
 }
